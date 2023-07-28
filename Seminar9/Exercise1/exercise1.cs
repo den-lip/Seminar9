203c40d0dab64a7d4e335e29d80fa8667fc918b8
den-lip <136602716+den-lip@users.noreply.github.com>
2023-07-28 21:22:48 +0300

    Add files via upload

---
+++ Seminar9/Exercise1/exercise1.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все ЧЕТНЫЕ 
+//натуральные числа в промежутке от 1 до N. Выполнить с помощью рекурсии.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 2);
+        Console.Write(count + " ");
+    }
+}
---
+++ Seminar9/Exercise3/exercise3.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+Console.Write("Введите переменную M = ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите переменную N = ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Функция равна {SumNumbers(M, N)}");
+Console.ReadLine();
+static int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                       
+    else if (M < N) return N + SumNumbers(M, N - 1); 
+    else return N + SumNumbers(M, N + 1);            
+}
